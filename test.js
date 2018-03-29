0d2db763548b450612ac407a6c5e96dd14faf98b
Arjun Lalwani <arjun11@uw.edu>
2018-03-29 10:52:19 -0700

    Update test.js

--- test.js
+++ test.js
@@ -2,12 +2,6 @@ var i = "testing";
 var x = "test 21111111";
 var j = "test 312";
 
-<<<<<<< HEAD
 for (var i = 0; i < 12; i++) {
-=======
-for (var i = 0; i < 15; i++) {
->>>>>>> 0b188d05cfefc95b68c15b0ea4d64047bf2a50d9
-
 	console.log("here");
-
 }
