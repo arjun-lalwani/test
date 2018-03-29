f44cd08736147e4259cb41427d6ea4331922f810
Arjun Lalwani <arjun11@uw.edu>
2018-03-29 10:53:41 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var i = "testing";
 var x = "test 21111111";
-var j = "test 31211111111111111111!";
+var j = "test 31";
 
-for (var i = 0; i < 15; i++) {
+for (var i = 0; i < 500000000; i++) {
 	console.log("here");
 }
